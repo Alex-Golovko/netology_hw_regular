c7158b3d6ddb46fd7587da88a3764198ddf22391
ya.golovko-alex2017@yandex.ru <Olga121082>
2021-08-29 18:10:48 +0300

    add encoding

--- regul.py
+++ regul.py
@@ -1,10 +1,10 @@
 from pprint import pprint
 # читаем адресную книгу в формате CSV в список contacts_list
 import csv
-with open("phonebook_raw.csv") as f:
+with open("phonebook_raw.csv", encoding= 'utf-8') as f:
   rows = csv.reader(f, delimiter=",")
   contacts_list = list(rows)
-pprint(contacts_list[0])
+pprint(len(contacts_list)
 
 # TODO 1: выполните пункты 1-3 ДЗ
 # ваш код
